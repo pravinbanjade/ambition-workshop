9aad22de57d75f23f0d0c65fa987170064392ca4
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-23 15:39:25 +0545

    removed unused files

--- assets/js/now-ui-kit.js
+++ assets/js/now-ui-kit.js
@@ -4,15 +4,6 @@
  * Now UI Kit - v1.2.0
  =========================================================
 
- * Product Page: https://www.creative-tim.com/product/now-ui-kit
- * Copyright 2018 Creative Tim (http://www.creative-tim.com)
-
- * Designed by www.invisionapp.com Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
  */
 
 
